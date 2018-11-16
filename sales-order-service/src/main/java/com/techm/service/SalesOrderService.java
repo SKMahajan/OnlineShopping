6f96600055cbffd04aa292484b8ddf71a2f43c39
Suchita <suchita.k.mahajan@gmail.com>
2018-11-16 15:13:33 +0530

    commit
    commit

--- sales-order-service/src/main/java/com/techm/service/SalesOrderService.java
+++ sales-order-service/src/main/java/com/techm/service/SalesOrderService.java
@@ -1,12 +1,16 @@
 package com.techm.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.techm.entity.Customer_SOS;
+import com.techm.entity.Item;
 import com.techm.entity.Order;
 
 @Service
 public interface SalesOrderService {
 	public String add(Order order);
 	public Customer_SOS addCustomer(Customer_SOS customer);
+	public List<Item> getItemList();
 }
